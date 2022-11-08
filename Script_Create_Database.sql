ad2f96a8321cdfc1c1d424249da09c3b6ada236b
Thiago Souza <lnsthiago@gmail.com>
2022-11-08 20:04:13 -0300

    Adicionando arquivo com o script de criação do banco de dados

---
+++ Script_Create_Database.sql
@@ -0,0 +1,10 @@
+﻿CREATE SCHEMA `fileaws`;
+
+USE `fileaws`;
+
+CREATE TABLE `files` (
+  `fileName` varchar(250) NOT NULL,
+  `fileSize` bigint NOT NULL,
+  `lastModified` datetime NOT NULL,
+  PRIMARY KEY (`fileName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; 
